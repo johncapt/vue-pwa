4f5a27bdea532ec17eff1dae47c4d1644eea494c
johncapt@insigtech.com.au <johncapt@insigtech.com.au>
2022-02-23 12:32:49 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import ToDo from './components/ToDo.vue'
+</script>
+
+<template>
+  <ToDo />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
